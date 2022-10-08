e1c9810b3b7244b336e677637bf5851f02d34fbd
martin-ngigi <martinwainaina001@gmail.com>
2022-10-08 22:48:54 +0300

    Initial commit

---
+++ src/main/kotlin/DocumentationKtor.kt
@@ -0,0 +1,22 @@
+//youtube link -> https://www.youtube.com/watch?v=uezSuUQt6DY&t=2234s
+//ktor github link -> https://github.com/philipplackner/KtorJwtAuth
+//
+//1.
+// 1.1
+// create a ktor project
+// Build system : Gradle Kotlin
+// Website: plcoding.com
+// Artifact: com.plcoding.ktor-jwt-auth
+// Ktor version: 2.1.2
+// Engine: Netty
+// Configuration in: HOCCON file
+//
+// 1.2
+// add plugins
+//  Authentication
+//  Authentication JWT
+//  Routing
+//  Content Negotiation
+//  Call login
+//  Kolinx.serialization
+//
